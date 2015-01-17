3feb8f058124f40cc402d3c059e587b52b991d14
Dimas Cyriaco <dimascyriaco@gmail.com>
2015-01-17 18:14:29 -0200

    Adding the README

---
+++ README.mk
@@ -0,0 +1,7 @@
+### Vimfiles
+
+1. git clone git@github.com:dimascyriaco/vimfiles
+2. cd vimfiles
+3. ln -s vimrc ~/.vimrc # Use the -sf flag if you want to overide your current .vimrc
+4. git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+5. vim +PluginInstall +qall
